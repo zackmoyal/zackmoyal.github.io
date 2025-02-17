7dc6405fe5e25b0212c61502465f26ff9c5779cf
Zack Moyal <zack.moyal@gmail.com>
2025-02-16 16:36:03 -0800

    Removed API key from repo

---
+++ Views/Home/StatsView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FortniteStatsAnalyzer.Views.Home
+{
+    public class StatsViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
